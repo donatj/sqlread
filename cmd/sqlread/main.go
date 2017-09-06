665c6999b8399af439e26f766974ce2c0bc47891
Jesse Donat <donatj@gmail.com>
2017-09-06 10:20:16 -0500

    include proof for now

--- cmd/sqlread/main.go
+++ cmd/sqlread/main.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	"log"
+	"net/http"
+	_ "net/http/pprof"
 	"os"
 
 	"github.com/donatj/sqlread"
 )
 
 func main() {
+
+	go func() {
+		log.Println(http.ListenAndServe("localhost:8080", nil))
+	}()
 	f, err := os.Open("/Users/jdonat/Desktop/myon_PRODUCTION_110915.sql")
 	// f, err := os.Open("test.sql")
 	if err != nil {
